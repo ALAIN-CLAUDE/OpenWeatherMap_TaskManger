eadafa700c6f484e9016d12b090ba9569183daac
Alain Claude <63712848+ALAIN-CLAUDE@users.noreply.github.com>
2024-02-04 23:38:31 +0000

    update

--- force-app/main/default/lwc/locationWeatherDisplay/locationWeatherDisplay.js
+++ force-app/main/default/lwc/locationWeatherDisplay/locationWeatherDisplay.js
@@ -11,7 +11,6 @@ export default class LocationWeatherDisplay extends LightningElement {
     wiredLocation({ error, data }) {
         if (data) {
             this.weatherInfoData = data.map(weatherInfo => ({
-              
                 id: weatherInfo.Id,
                 iconUrl: weatherInfo.Icon__c
                     ? `https://openweathermap.org/img/wn/${weatherInfo.Icon__c}.png`
@@ -24,11 +23,8 @@ export default class LocationWeatherDisplay extends LightningElement {
                 Maximum_Temperature: weatherInfo.Maximum_Temperature__c ? weatherInfo.Maximum_Temperature__c : '',
                 Feels_Like: weatherInfo.Feels_Like__c ? weatherInfo.Feels_Like__c : '',
                 LocationName : weatherInfo.Name,
-                // Add other properties as needed
+
             }));
-            console.log('this.data===> '+JSON.stringify(data));
-            console.log('this.locationData===> '+JSON.stringify(this.weatherInfoData));
-            console.log('this.locationData length===> '+this.weatherInfoData.length);
         } else if (error) {
             this.error = error;
             console.error('Error loading location weather:', error);
@@ -39,7 +35,7 @@ export default class LocationWeatherDisplay extends LightningElement {
         return this.weatherInfoData && this.weatherInfoData.length > 0;
     }
 
-    get LocationName(){
+    get LocationName() {
         return this.weatherInfoData[0].LocationName;
     }
 }
